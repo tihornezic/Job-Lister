fbf8bde9a0db6c419dcaef96b48287310306e1b4
Tihor Nezic <tihor.nezic@gmail.com>
2020-12-09 13:12:16 +0100

    config set to getenv for Heroku

--- config/config.php
+++ config/config.php
@@ -2,16 +2,16 @@
 
 // db params
 
-define("DB_HOST", "localhost");
+/* define("DB_HOST", "localhost");
 define("DB_USER", "root");
 define("DB_PASS", "");
-define("DB_NAME", "job_lister");
+define("DB_NAME", "job_lister"); */
 
 
-/* define("DB_HOST", getenv('DB_HOST'));
+define("DB_HOST", getenv('DB_HOST'));
 define("DB_USER", getenv('DB_NAME'));
 define("DB_PASS", getenv('DB_PASS'));
-define("DB_NAME", getenv('DB_USER')); */
+define("DB_NAME", getenv('DB_USER'));
 
 define("SITE_TITLE", "Job Lister")
 
